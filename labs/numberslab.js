e724e9dc95d1c309fb2beb5f57d63627f5f1a7b5
awais nawaz <arib4ariz@gmail.com>
2026-02-08 21:20:27 -0500

    lab assignment numbers

---
+++ labs/numberslab.js
@@ -0,0 +1,28 @@
+// Task 1
+
+let numSeparator1 = 34038;
+console.log(numSeparator1);
+
+let numSeparator2 = 45838.8734;
+console.log(numSeparator2);
+
+let numSeparator3 = 0b011000101011;
+console.log(numSeparator3);
+
+let numSeparator4 = 0x174c0;
+console.log(numSeparator4);
+
+let numSeparator5 = -895612
+console.log(numSeparator5);
+
+// Tast 2
+
+
+let binaryValue = 0b011101;
+console.log(` ${binaryValue}`);
+
+let octalValue = 0o35;
+console.log(` ${octalValue}`);
+
+let hexValue = 0x1d;
+console.log(` ${hexValue}`);
