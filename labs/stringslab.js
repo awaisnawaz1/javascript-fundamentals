dbe55e7650b1a05c657842384c7064cce6993327
awais nawaz <arib4ariz@gmail.com>
2026-02-08 21:15:13 -0500

    2nd Lab Assignment String

---
+++ labs/stringslab.js
@@ -0,0 +1,19 @@
+//Task 1 
+
+let message = "  This statement has empty spaces. ";
+console.log(message.includes('pty'));
+
+let study  = " This statement has empty spaces. " ;
+console.log(study.charAt(12));
+
+let work = "   This statement has empty spaces.    ";
+console.log(work.trim());
+
+
+let game = " THIS STATEMENT HAS EMPTY SPACES. ";
+console.log(game.toLowerCase());
+
+//Task 2
+
+let phrase = " Jack and Jill went up the hill to fetch a pail of water. \nJack fell down and broke his crown \nand jill came tumbling after." ;
+console.log(phrase);
