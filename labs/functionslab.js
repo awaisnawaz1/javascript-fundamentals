99d9736f39dba9b457d67cb27bc6c35cc1c7ffda
awais nawaz <arib4ariz@gmail.com>
2026-02-19 09:04:49 -0500

    function lab update

--- labs/functionslab.js
+++ labs/functionslab.js
@@ -2,12 +2,12 @@
 This function expression should return both strings using the + sign. Be sure to test the function
 expression using a console.log statement. */
 
-function strings(num1,num2)
-{
-    return num1+num2;
-}
-let value=strings("Hello", "World");
-console.log(value);
+    const expression = function(able1, able2) {
+    return able1 + able2; 
+};
+      console.log(expression("Lab ", "Assignment"));
+
+
 
     /* Task 2  == Write a function that will have 2 parameters. One of the parameters will have a default value of 15. This
 function should have a return that adds both parameters together. Be sure to test this function using 2
@@ -23,8 +23,9 @@ console.log(assignment(12));
 /* Task 3 ==  Write an arrow function based on Task 1. Be sure to test this function
  using a console.log statement.  */
 
-{
-  let value50Arrow = value => value + 50;
 
-  console.log(value50Arrow(10));
-}
+const dive3Arrow = (able1, able2) => 10 + 5;
+
+
+console.log(dive3Arrow());
+
